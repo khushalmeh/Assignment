785d471918f8f31e9d0a5870c2897982dafde4cf
khushalmeh <khushalmehta96@gmail.com>
2023-01-31 15:45:48 +0530

    Game

---
+++ js/word.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word: "kohli",
+        hint: "A cricketer"
+    },
+    {
+        word: "india",
+        hint: "A country"
+    },
+    {
+        word: "arijit",
+        hint: "A Singer"
+    },
+    {
+        word: "bmw",
+        hint: "A Car"
+    },
+    {
+        word: "reactjs",
+        hint: "A Javascript library"
+    },
+]
